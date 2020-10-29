73afaf48587deb5a871fa100ce4f99ed162e5f7e
zhouyijun <zhou495248579@hotmail.com>
2020-10-29 17:06:09 +0800

    数据封装到json中使用依赖注入http获取

--- app/test-case/list/list.component.js
+++ app/test-case/list/list.component.js
@@ -1,7 +1,12 @@
 angular.module("list").component("list", {
   templateUrl: "list/list.html", // url地址是从项目根目录下算起的
-  controller: function () {
+  controller: function ($http) {
     "use strict";
-    this.list = ["list1"];
+    $http.get("./list/list.json").then((response) => {
+      console.log(response);
+      this.list = response.data.phones;
+    });
+    this.query = "";
+    this.orderProp = "";
   },
 });
