f02ec8beb7d9e90c0e1f0b163aa9af24b5f6e734
zhouyijun <zhou495248579@hotmail.com>
2020-10-31 12:21:03 +0800

    test controller

--- app/test-case/a.js
+++ app/test-case/a.js
@@ -1,6 +1,20 @@
 angular
   .module("module1", [])
-  .controller("controller", function () {})
+  .controller("controller", function ($scope) {
+      $scope.count = 10;
+      $scope.price = 5;
+    $scope.compute = function () {
+
+      return $scope.count * $scope.price;
+    };
+    $scope.login = function () {
+        if($scope.count === $scope.price) {
+            console.log('ok')
+        } else  {
+            console.error('erroe')
+        }
+    }
+  })
   .directive("directive", function () {
     return {
       restrict: "AE",
