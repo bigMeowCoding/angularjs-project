df01a16bd0be07867e5dc566c7bfc8cbdf7a2704
zhouyijun <zhou495248579@hotmail.com>
2020-10-31 14:23:36 +0800

    $time out test

--- app/test-case/a.js
+++ app/test-case/a.js
@@ -1,19 +1,28 @@
 angular
   .module("module1", [])
-  .controller("controller", function ($scope) {
-      $scope.count = 10;
-      $scope.price = 5;
+  .controller("controller", function ($scope,$timeout) {
+    $scope.count = 10;
+    $scope.price = 5;
     $scope.compute = function () {
-
       return $scope.count * $scope.price;
     };
     $scope.login = function () {
-        if($scope.count === $scope.price) {
-            console.log('ok')
-        } else  {
-            console.error('erroe')
-        }
-    }
+      if ($scope.count === $scope.price) {
+        console.log("ok");
+      } else {
+        console.error("erroe");
+      }
+    };
+    $scope.later = function () {
+      // setTimeout(() => {
+      //   $scope.$apply(function () {
+      //     $scope.message = "message";
+      //   });
+      // }, 1000);
+      $timeout(() => {
+        $scope.message = "$timer out message";
+      }, 3000);
+    };
   })
   .directive("directive", function () {
     return {
