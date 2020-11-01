c7826d19746d4bcb992a34485a23f93b7f5865b7
zhouyijun <zhou495248579@hotmail.com>
2020-11-01 11:11:52 +0800

    指令

--- app/app.js
+++ app/app.js
@@ -1,14 +1,43 @@
-"use strict";
-
-// Declare app level module which depends on views, and core components
 angular
-  .module("myApp", ["ngRoute", "myApp.view1", "myApp.view2", "myApp.version"])
-  .config([
-    "$locationProvider",
-    "$routeProvider",
-    function ($locationProvider, $routeProvider) {
-      $locationProvider.hashPrefix("!");
-
-      $routeProvider.otherwise({ redirectTo: "/view1" });
-    },
-  ]);
+  .module("module1", ["ui.router", "ngMessages"])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state("form", {
+        url: "/form",
+        templateUrl: "./form/form.html",
+      })
+      .state("contact", {
+        url: "/contact",
+        templateUrl: "./contact.html",
+      });
+  })
+  .controller("controller", function ($scope, $timeout) {
+    $scope.count = 10;
+    $scope.price = 5;
+    $scope.compute = function () {
+      return $scope.count * $scope.price;
+    };
+    $scope.login = function () {
+      if ($scope.count === $scope.price) {
+        console.log("ok");
+      } else {
+        console.error("erroe");
+      }
+    };
+    $scope.later = function () {
+      // setTimeout(() => {
+      //   $scope.$apply(function () {
+      //     $scope.message = "message";
+      //   });
+      // }, 1000);
+      $timeout(() => {
+        $scope.message = "$timer out message";
+      }, 3000);
+    };
+  })
+  .directive("directive", function () {
+    return {
+      restrict: "AE",
+      templateUrl: "my-customer.html",
+    };
+  });
